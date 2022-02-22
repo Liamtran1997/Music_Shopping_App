cdda140b9f19bdd3c329471616b33d54f4652842
Liamtran1997 <workwithtuan1908@gmail.com>
2022-02-22 16:52:43 +0700

    Update LineItem and Cart

---
+++ app/models/cart.rb
@@ -0,0 +1,13 @@
+class Cart < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
+  def add_instruments(instrument)
+    current_item = line_items.find_by(instrument_id :instrument_id)
+    if current_item
+      current_item.increment(:quantity)
+    else
+      current_item = line_items.build(instrument_id :instrument_id)
+    end
+    current_item
+  end
+end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,11 +2,12 @@ class Instrument < ApplicationRecord
   mount_uploader :image, ImageUploader
   serialize :image, JSON # If you use SQLite, add this line
   belongs_to :user
+  has_many :line_items
 
   validates :title, :brand, :price, :model, :name, presence: true
-  validates :description, length: {maximum: 1000, minimum: 50, too_long: "%{count Characters is the maximum allowed}"}
+  validates :description, length: {maximum: 1000, too_long: "%{count Characters is the maximum allowed}"}
   validates :title, length: {maximum: 140, too_long: "%{count Characters is the maximum allowed}"}
-  validates :price, numericality: {only_integer: true }, length: {minimum: 7}
+  validates :price, length: {maximum: 7}
 
   BRAND = %w{Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
   NAME = %w{Trumpet Tuba Guitar Piano Cello Violin}
